21c8f35f65cac4b5c440c461fe486b595f8c44d8
true.xrstn <mr.chareev@mail.ru>
2023-05-03 04:22:21 +0800

    1

---
+++ src/main/java/com/example/hm15/Hm15Application.java
@@ -0,0 +1,13 @@
+package com.example.hm15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm15Application.class, args);
+    }
+
+}
